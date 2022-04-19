a9cfde4aea2d3d1cb309401b8184cd18cf6e2210
teposh <teposh@users.noreply.github.com>
2022-04-19 22:45:50 +0300

    chore: update postman/sprint1.json

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
 /**
- * Исключение для ошибки при кастомной валидации.
+ * Исключение для ошибки при самостоятельной валидации.
  */
 public class ValidationException extends RuntimeException {
     public ValidationException(String s) {
