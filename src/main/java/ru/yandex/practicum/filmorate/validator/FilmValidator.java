400ddc46ca026250546e0c87a52a572e25d406b1
teposh <teposh@users.noreply.github.com>
2022-05-30 18:15:00 +0300

    fix: remove unnecessary validation rules

--- src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/FilmValidator.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.validator;
 
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.utils.IdProvider;
 
@@ -13,14 +12,11 @@ public class FilmValidator {
      *
      * @param film валид. фильм
      * @return новый объект фильма
-     * @throws ValidationException - если фильм не валиден.
      */
     public static Film validate(Film film) {
         if (film.getId() == null) {
             film = film.toBuilder().id(IdProvider.getNextLongId(Film.class)).build();
         }
-
-        if (film.getId() < 0) throw new ValidationException("film id must be greater than zero");
 
         return film;
     }
