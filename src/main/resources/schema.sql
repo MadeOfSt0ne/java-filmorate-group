3788e7a86892e2dfd1076f8c5ba7f25e0c5653e7
teposh <teposh@users.noreply.github.com>
2022-05-30 20:15:00 +0300

    chore: add data.sql | some code cleanup

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS mpa
+(
+    mpa_id BIGINT auto_increment NOT NULL PRIMARY KEY,
+    title  VARCHAR(255)          NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS films
 (
     film_id      BIGINT auto_increment NOT NULL PRIMARY KEY,
@@ -5,7 +11,8 @@ CREATE TABLE IF NOT EXISTS films
     description  VARCHAR(200)          NOT NULL,
     release_date DATE                  NOT NULL,
     duration     BIGINT                NOT NULL,
-    mpa_id       INT                   NULL
+    mpa_id       INT                   NULL,
+    FOREIGN KEY (mpa_id) REFERENCES mpa (mpa_id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS genres
@@ -18,15 +25,9 @@ CREATE TABLE IF NOT EXISTS film_genres
 (
     film_id  BIGINT NOT NULL,
     genre_id BIGINT NOT NULL,
-    FOREIGN KEY (film_id) references films (film_id),
-    FOREIGN KEY (genre_id) references genres (genre_id),
+    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres (genre_id) ON DELETE CASCADE,
     PRIMARY KEY (film_id, genre_id)
-);
-
-CREATE TABLE IF NOT EXISTS mpa
-(
-    mpa_id BIGINT auto_increment NOT NULL PRIMARY KEY,
-    title  VARCHAR(255)          NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
@@ -42,7 +43,8 @@ CREATE TABLE IF NOT EXISTS friends
 (
     user_id   BIGINT NOT NULL,
     friend_id BIGINT NOT NULL,
-    approved  BOOLEAN default false,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES users (user_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, friend_id)
 );
 
@@ -50,5 +52,7 @@ CREATE TABLE IF NOT EXISTS likes
 (
     user_id BIGINT NOT NULL,
     film_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (film_id) REFERENCES films (film_id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, film_id)
 );
